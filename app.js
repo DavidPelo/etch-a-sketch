7d329dc93646d9da3ff4b97333c769f514d4e70a
David Pelo <davidwpelo@gmail.com>
2021-11-11 08:15:12 -0600

    Styled etch-a-sketch features

--- app.js
+++ app.js
@@ -1,45 +1,69 @@
 const etchContainer = document.querySelector('.etch-container');
 const clearBtn = document.querySelector('.clear-sketch');
+const sizeSlider = document.querySelector('#size');
+const colorSelector = document.querySelector('#color');
 
-let defaultGridSize = 16;
+//default values
+sizeSlider.value = 16;
+colorSelector.value = 'black';
+let gridSize = 16;
+let color = colorSelector.value; //black
 
-loadEtchContainer(defaultGridSize);
+//load etch-a-sketch and event listeners once elements are created
+loadEtchContainer(gridSize);
 loadEventListeners();
 
-function loadEtchContainer(size) {
-    console.log(size);
-    etchContainer.innerHTML = '';
+function loadEtchContainer(gridSize) {
+    etchContainer.innerHTML = "";
 
-    for(var i = 0; i < size; i++) {
+    //add a row, for each row, add gridSize columns
+    for(var i = 0; i < gridSize; i++) {
         newRow = document.createElement('div');
         newRow.classList.add('row');
-        console.log('new row added');
 
-        for(var j = 0; j < size; j++) {
+        for(var j = 0; j < gridSize; j++) {
             newColumn = document.createElement('div');
             newColumn.classList.add('column');
             newRow.appendChild(newColumn);
-            console.log('new column added');
         }
 
         etchContainer.appendChild(newRow);
     }
+
+    //load event listeners for each ethc-a-sketch square div
+    loadGridEventListeners();
 }
 
+//event listeners for inputs
 function loadEventListeners() {
+    clearBtn.addEventListener('click', clearSketch);
+    sizeSlider.addEventListener('input', changeGridSize);
+    colorSelector.addEventListener('change', changeColor);
+}
+
+//load each event listener for each grid square
+function loadGridEventListeners() {
     let etchSquares = etchContainer.querySelectorAll('.column');
     etchSquares.forEach(square => square.addEventListener('mouseover', colorSquare));
-    clearBtn.addEventListener('click', clearSketch);
 }
 
+//colors the square when colorSqaure is called by the event listener
 function colorSquare() {
-    this.style.backgroundColor = 'black';
+    this.style.backgroundColor = color;
 }
 
+//clears sketch
 function clearSketch() {
     let etchSquares = etchContainer.querySelectorAll('.column');
     etchSquares.forEach(square => square.style.backgroundColor = 'white')
-    let gridSize = prompt('What size grid would you lke?');
-    
-    loadEtchContainer(gridSize);
+}
+
+//changes grid size by calling loadEtchContainer with input value
+function changeGridSize(e) {
+    loadEtchContainer(e.target.value);
+}
+
+//set global color variable value
+function changeColor(e) {
+    color = e.target.value;
 }
